7de11751f545de8ffd64973ca697be6c158354e0
3mu-thoni <muthonimegg@gmail.com>
2026-01-08 11:36:58 +0300

    Initial portfolio commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text",
+    {
+        strings:["Software Engineer", "Cloud Engineer", "JavaScript Engineer", "UI/AX Desiner"],
+        backSpeed:100,
+        typedSpeed:100,
+        backDelay:100,
+        loop:true
+
+    }
+
+)
